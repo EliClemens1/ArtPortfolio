6c3c999493d477085e44a70d3e3bfad75f7b1030
Eli Clemens <ec02162129@otc.edu>
2026-03-16 11:58:23 -0500

    Images and data in 'artworks' array are bound to card template using v-for

--- Js/components/main_page.js
+++ Js/components/main_page.js
@@ -1,60 +1,67 @@
 const MainPage = {
-//Create vue component called MainPage
-//Template contains HTML for the main page of the artist portfolio website use backticks "`" for multi line 
+    data() {
+        return {
+
+            // Array containing artwork objects
+            artworks: [
+                {
+                    title: "Ocean Foam",
+                    artist: "E Like Lemons",
+                    date: "2024",
+                    medium: "Watercolor",
+                    dimensions: "24 x 36 in",
+                    image: "placeholderImages/wetCat.jpg"
+                },
+                {
+                    title: "Finals Week",
+                    artist: "Rick Sanchez",
+                    date: "2023",
+                    medium: "Acrylic",
+                    dimensions: "18 x 24 in",
+                    image: "placeholderImages/Frustrated.jpg"
+                },       
+                {
+                    title: "Graduated!",
+                    artist: "Rick Clemens",
+                    date: "2028",
+                    medium: "Crayon",
+                    dimensions: "18 x 24 in",
+                    image: "placeholderImages/CoolCat.jpg"
+                },
+            ]
+        }
+    },
     template: `
     <div class="main-page">
 
         <h2>Main Page</h2>
 
+        <!-- Search bar, upload button, and filter button -->
         <div class="actions">
             <input type="text" placeholder="Search artwork...">
-
             <button>Upload</button>
-
             <button>Filter</button>
-
         </div>
 
         <!-- List of artworks will go here -->
         <div class="artwork-list">
+        
+        <!-- V-For to loop through 'artworks' array and display them -->
+            <div class="art-card" v-for="art in artworks" :key="art.title">
+                <div class="art-image">
+                    <img :src="art.image" alt="Artwork Image">
+                </div>
 
-            <div class="art-card">
-
-                <div class="art-image">Image</div>
-
+                <!-- Artwork information -->
                 <div class="art-info">
-                    <h3>Artwork Title</h3>
-                    <p>Artist Name</p>
-                    <p>Date</p>
-                    <p>Medium</p>
-                    <p>Dimensions</p>
+                    <h3>{{ art.title }}</h3>
+                    <p>{{ art.artist }}</p>
+                    <p>{{ art.date }}</p>
+                    <p>{{ art.medium }}</p>
+                    <p>{{ art.dimensions }}</p>
                 </div>
             </div>
             
-            <div class="art-card">
-
-                <div class="art-image">Image</div>
-
-                <div class="art-info">
-                    <h3>Artwork Title</h3>
-                    <p>Artist Name</p>
-                    <p>Date</p>
-                    <p>Medium</p>
-                    <p>Dimensions</p>
-                </div>
-            </div>            
-            
-            <div class="art-card">
-
-                <div class="art-image">Image</div>
-
-                <div class="art-info">
-                    <h3>Artwork Title</h3>
-                    <p>Artist Name</p>
-                    <p>Date</p>
-                    <p>Medium</p>
-                    <p>Dimensions</p>
-                </div>
-            </div>
-        </div>`
+        </div>
+    </div>`
 }
