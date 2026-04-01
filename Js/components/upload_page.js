d13d0be78461da14ef56020ecdb520749cc3ba44
Emma <eb0980835@otc.edu>
2026-04-01 00:43:25 -0500

    added comments to upload page component

--- Js/components/upload_page.js
+++ Js/components/upload_page.js
@@ -39,16 +39,17 @@ export default {
             </form>
         </div>
     `,
-
+// I went ahead and added my initials to comments that don't necessarily explain the code and can be removed later (besides this one). - Emma
     data() {
         return {
+            // The variable names need to be changed so that they match with the database - E.B.
             artTitle: '',
             artDesc: '',
             artDate: '',
             artPrint: '',
             artLocation: '',
             artMedium: '',
-            artHeight: '',
+            artHeight: '', // I separated all of the dimensions into different inputs since I figured that'd be easier than having the user type out each x, but if we want to change that we can - E.B.
             artWidth: '',
             artDepth: '',
             artImages: []
@@ -56,28 +57,30 @@ export default {
     },
 
     methods: {
+        // This is just a temp method that I'm using for testing -E.B.
         artSubmit() {
             alert(this.artTitle + ',' + this.artDesc + ',' + this.artDate + ',' + this.artPrint + ',' + this.artLocation + ',' + this.artMedium + ',' + this.artHeight + 'x' + this.artWidth + 'x' + this.artDepth + ',' + this.artImages)
         },
 
         previewImages() {
             var artUpload = document.getElementById("artImages");
-            if (typeof (FileReader) != undefined) {
+            if (typeof (FileReader) != undefined) { // Only runs if file(s) were chosen
                 var artPreview = document.getElementById("imgsPreview");
                 artPreview.innerHTML = "";
-                var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.jpg|.jpeg|.gif|.png|.bmp)$/;
-                for (var i = 0; i < artUpload.files.length; i++) {
+                var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.jpg|.jpeg|.gif|.png|.bmp)$/; // Regex for image file types
+                // I'm not sure what file types we want to be able to be uploaded, so we can change them however we need. I just copied the Regex from the example I found - E.B.
+                for (var i = 0; i < artUpload.files.length; i++) { // Loops through the file list that the input tag creates
                     var artFile = artUpload.files[i];
-                    if (regex.test(artFile.name.toLowerCase())) {
+                    if (regex.test(artFile.name.toLowerCase())) { // Tests to make sure the file matches the Regex
                         var reader = new FileReader;
-                        reader.onload = function (e) {
+                        reader.onload = function (e) { // Creates an image tag to display the image on the page
                             var img = document.createElement("IMG");
                             img.src = e.target.result;
                             artPreview.appendChild(img);
                         }
                         reader.readAsDataURL(artFile);
                     }
-                    else {
+                    else { // Runs if file type is invalid
                         alert(artFile.name + "is not a valid image file.");
                         artPreview.innerHTML = "";
                         return false;
